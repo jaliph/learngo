589f65453941b2d85cfbe41f440c7fbdf441161d
Akash Chandra <akash.chandra.4u@gmail.com>
2023-10-26 14:50:09 +0530

    Create min-score-triangulation.go

---
+++ leetcode/dp/mcm/min-score-triangulation.go
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/minimum-score-triangulation-of-polygon/solutions/286753/c-with-picture/
+
+package dp
+
+func minScoreTriangulation(values []int) int {
+
+	Min := func(a int, b int) int {
+		if a < b {
+			return a
+		}
+		return b
+	}
+	const INF int = 1e15
+	n := len(values)
+	R := n
+	C := n
+	dp := make([][]int, R)
+	rows := make([]int, R*C)
+	for i := range dp {
+		dp[i] = rows[i*C : (i+1)*C]
+	}
+
+	for i := n - 2; i >= 0; i++ {
+		for j := i + 2; j < n; j++ {
+			dp[i][j] = INF
+			for k := i + 1; k < j; k++ {
+				dp[i][j] = Min(dp[i][j], dp[i][k]+(values[i]*values[k]*values[j])+dp[k][j])
+			}
+		}
+	}
+
+	return dp[0][n-1]
+}
