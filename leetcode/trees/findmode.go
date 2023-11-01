212a371f34d0f2d2cdc8965e5dff1c4731f1d2e6
Akash Chandra <akash.chandra.4u@gmail.com>
2023-11-02 00:45:25 +0530

    Fixed FindModes

--- leetcode/trees/findmode.go
+++ leetcode/trees/findmode.go
@@ -49,3 +49,43 @@ func FindMode(root *TreeNode) []int {
 	inorder(root)
 	return result
 }
+
+func FindMode_Iterative(root *TreeNode) []int {
+	result := []int{}
+	maxStreak := 0
+	currStreak := 0
+	currMode := 0
+	stack := []*TreeNode{}
+
+	curr := root
+
+	for curr != nil || len(stack) > 0 {
+		for curr != nil {
+			stack = append(stack, curr)
+			curr = curr.Left
+		}
+
+		// pop for processing
+		popped := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		// find modes
+		if popped.Val == currMode {
+			currStreak++
+		} else {
+			currStreak = 1
+			currMode = popped.Val
+		}
+
+		if currStreak == maxStreak {
+			result = append(result, currMode)
+		} else if maxStreak < currStreak {
+			maxStreak = currStreak
+			result = []int{currMode}
+		}
+
+		// check the right node
+		curr = popped.Right
+	}
+	return result
+}
