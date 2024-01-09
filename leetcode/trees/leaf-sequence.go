99e69f03d033eec30f19b5fa9ed7d5d266b3ce7c
Akash Chandra <akachan1@in.ibm.com>
2024-01-09 10:55:09 +0530

    Create leaf-sequence.go

---
+++ leetcode/trees/leaf-sequence.go
@@ -0,0 +1,48 @@
+package trees
+
+// https://leetcode.com/problems/leaf-similar-trees
+
+/* Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func leafSimilar(root1 *TreeNode, root2 *TreeNode) bool {
+	l1 := getLeafSequence(root1)
+	l2 := getLeafSequence(root1)
+
+	if len(l1) != len(l2) {
+		return false
+	}
+
+	for i, v := range l1 {
+		if l2[i] != v {
+			return false
+		}
+	}
+
+	return true
+}
+
+func getLeafSequence(node *TreeNode) []int {
+	leafSequence := []int{}
+
+	var dfs func(*TreeNode)
+	dfs = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+
+		dfs(node.Left)
+		if node.Left == nil && node.Right == nil {
+			leafSequence = append(leafSequence, node.Val)
+		}
+		dfs(node.Right)
+	}
+	// call the function
+	dfs(node)
+	return leafSequence
+}
