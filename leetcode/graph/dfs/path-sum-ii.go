a1de9822e8ba6c7e9debabe5aa61bd22b5db745c
Akash Chandra <akash.chandra.4u@gmail.com>
2024-06-12 19:12:16 +0530

    Create path-sum-ii.go

---
+++ leetcode/graph/dfs/path-sum-ii.go
@@ -0,0 +1,35 @@
+package graph
+
+// https://leetcode.com/problems/path-sum-ii/
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func pathSum(root *TreeNode, targetSum int) [][]int {
+	res := [][]int{}
+
+	var dfs func(*TreeNode, int, []int)
+	dfs = func(node *TreeNode, sum int, path []int) {
+		if node == nil {
+			return
+		}
+
+		if sum < 0 {
+			return
+		}
+
+		if node.Left == nil && node.Right == nil {
+			if targetSum-node.Val == 0 {
+				res = append(res, path)
+			}
+		}
+
+		dfs(node.Left, targetSum-node.Val, append(path, node.Val))
+		dfs(node.Right, targetSum-node.Val, append(path, node.Val))
+
+	}
+
+	dfs(root, targetSum, []int{})
+}
